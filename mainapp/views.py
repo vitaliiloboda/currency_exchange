469e997a76345f827978d8abd9940257d4135be8
VitaliiLoboda <vitalii.loboda@gmail.com>
2022-07-30 15:22:13 +0300

    removed requirements.txt

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,22 +4,23 @@ import os
 
 
 def index(request):
-    to = 'USD'
-    from_currency = 'EUR'
-    amount = 3500
+    # to = 'USD'
+    # from_currency = 'EUR'
+    # amount = 3500
+    #
+    # url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from_currency}&amount={amount}"
+    #
+    # payload = {}
+    # headers = {
+    #     # "apikey": os.getenv('APIKEY')
+    # }
+    #
+    # response = requests.request("GET", url, headers=headers, data=payload)
+    # data = response.json()
+    # status_code = response.status_code
+    # result_str = response.text
+    # result_amount = data['result']
 
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from_currency}&amount={amount}"
-
-    payload = {}
-    headers = {
-        # "apikey": os.getenv('APIKEY')
-    }
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-    data = response.json()
-    status_code = response.status_code
-    result_str = response.text
-    result_amount = data['result']
-
+    result_amount = 'result'
     context = {'result_amount': result_amount}
     return render(request, 'mainapp/index.html', context)
