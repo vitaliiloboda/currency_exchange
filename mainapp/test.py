714c882f0ce45d535b9e3329668e68718cf506a9
VitaliiLoboda <vitalii.loboda@gmail.com>
2022-07-30 17:30:53 +0300

    remove apikey

--- mainapp/test.py
+++ mainapp/test.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 
 url = f"https://api.apilayer.com/exchangerates_data/latest?symbols"
@@ -5,7 +7,7 @@ url = f"https://api.apilayer.com/exchangerates_data/latest?symbols"
 
 payload = {}
 headers = {
-  "apikey": "wNKgZXAAv3DddNIVhEpu1ZV7X8xGAvwZ"
+    "apikey": os.getenv('APIKEY')
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
@@ -13,3 +15,26 @@ response = requests.request("GET", url, headers=headers, data=payload)
 status_code = response.status_code
 result = response.text
 print(result)
+
+
+def index(request):
+    to = 'USD'
+    from_currency = 'EUR'
+    amount = 3500
+
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from_currency}&amount={amount}"
+
+    payload = {}
+    headers = {
+        # "apikey": os.getenv('APIKEY')
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.json()
+    status_code = response.status_code
+    result_str = response.text
+    result_amount = data['result']
+
+    result_amount = 'result'
+    context = {'result_amount': result_amount}
+    # return render(request, 'mainapp/index.html', context)
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,22 +4,14 @@ import os
 
 
 def index(request):
-    to = 'USD'
-    from_currency = 'EUR'
-    amount = 3500
 
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from_currency}&amount={amount}"
+    url = f"https://api.apilayer.com/exchangerates_data/latest?symbols"
 
-    payload = {}
     headers = {
-        # "apikey": os.getenv('APIKEY')
+        'apikey': os.getenv('APIKEY')
     }
 
-    response = requests.request("GET", url, headers=headers, data=payload)
-    data = response.json()
-    status_code = response.status_code
-    result_str = response.text
-    result_amount = data['result']
+    response = requests.request('GET', url, headers=headers)
 
     result_amount = 'result'
     context = {'result_amount': result_amount}
